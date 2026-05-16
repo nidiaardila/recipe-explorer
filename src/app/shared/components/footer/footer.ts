cf66605948053f62fdddf1d3e744b7cb1d92bb1d
Nidia Ardila <116224096+nidiaardila@users.noreply.github.com>
2026-05-16 21:05:27 +0000

    docs: add Recipe Explorer README

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
+  styleUrl: './footer.scss'
 })
 export class Footer {
-
+  currentYear = new Date().getFullYear();
 }
